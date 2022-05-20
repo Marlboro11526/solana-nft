fcc22bcb3ca21a181564ef776d8848ac6889a5d4
Marlboro11526 <RichardJohnston33@outlook.com>
2022-05-20 14:38:02 +0530

    import libs

--- src/program/src/lib.rs
+++ src/program/src/lib.rs
@@ -1,3 +1,5 @@
-fn main() {
-    println!("Hello, world!");
-}
+use anchor_lang::prelude::*;
+use anchor_lang::solana_program::program::invoke;
+use anchor_spl::token::mint_to;
+use anchor_spl::token::{MintTo, Token};
+use mpl_token_metadata::instruction::create_metadata_accounts_v2;
