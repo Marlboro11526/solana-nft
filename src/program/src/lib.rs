1138992cd20df25ec47840b0cc176f2939dfbb0e
Marlboro11526 <RichardJohnston33@outlook.com>
2022-05-29 18:58:31 +0530

    Update lib.rs
    The compiler is capable of providing basic implementations for some traits via the #[derive] attribute. These traits can still be manually implemented if a more complex behavior is required.
    
    Implements an Accounts deserializer on the given struct, applying any constraints specified via inert #[account(..)] attributes upon deserialization.

--- src/program/src/lib.rs
+++ src/program/src/lib.rs
@@ -72,7 +72,7 @@ pub mod nft_minting_contract {
                 None,
                 None,
             ),
-            &accounts
+            &accounts,
         );
         if let Err(_) = result {
             return Err(error!(ErrorCode::MetadataCreateFailed));
@@ -81,5 +81,43 @@ pub mod nft_minting_contract {
         Ok(())
     }
 }
-    }
+#[derive(Accounts)]
+pub struct MintNFT<'info> {
+    #[account(mut)]
+    pub mint_authority: Signer<'info>,
+
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    #[account(mut)]
+    pub mint: UncheckedAccount<'info>,
+
+    pub token_program: Program<'info, Token>,
+
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    #[account(mut)]
+    pub metadata: UncheckedAccount<'info>,
+
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    #[account(mut)]
+    pub token_account: UncheckedAccount<'info>,
+
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    pub token_metadata_program: UncheckedAccount<'info>,
+
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    #[account(mut)]
+    pub payer: AccountInfo<'info>,
+
+    pub system_program: Program<'info, System>,
+
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    pub rent: AccountInfo<'info>,
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("NFT mint failed!")]
+    MintFailed,
+
+    #[msg("Metadata account creation failed!")]
+    MetadataCreateFailed,
 }
