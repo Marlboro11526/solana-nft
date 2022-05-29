5c0851660562081d9dce975996c373fab5e90cc4
Marlboro11526 <RichardJohnston33@outlook.com>
2022-05-29 18:40:39 +0530

    import metaplex

--- src/program/src/lib.rs
+++ src/program/src/lib.rs
@@ -3,3 +3,6 @@ use anchor_lang::solana_program::program::invoke;
 use anchor_spl::token::mint_to;
 use anchor_spl::token::{MintTo, Token};
 use mpl_token_metadata::instruction::create_metadata_accounts_v2;
+
+#[program]
+pub mod nft_minting_contract {}
